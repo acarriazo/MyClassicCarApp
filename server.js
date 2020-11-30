472d632c7726b8f7db6014707413ab21be7511ba
acarriazo <antonio.carriazo@gmail.com>
2020-11-30 23:50:27 +0100

    first commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/myclassiccarapp'));
+app.use(express.static(__dirname+'/dist/MyClassicCarApp'));
 /* importante poner '/*' para utilizar correctamente el routing interno de angular
 y evitar errores de redireccionamiento*/
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/myclassiccarapp/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/MyClassicCarApp/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
